e28324e9356a625bfce373887e15f66eae7b2ee5
Kayla Melton <kannmelton2@gmail.com>
2020-02-29 10:06:18 -0600

    utils and tank file

---
+++ javascripts/components/tank.js
@@ -0,0 +1,9 @@
+import utils from '../helpers/utils.js';
+//import personData from '../helpers/data/personData.js';
+
+const tankBuilder = () => {
+    let domString = 'help';
+    utils.printToDom('tank', domString);
+};
+
+export default { tankBuilder };
